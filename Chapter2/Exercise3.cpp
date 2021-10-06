513c85a25c4b05e7238301f2b983cfcff22e71de
taniguchi <hogehoge>
2021-10-06 17:15:52 +0900

    #6 fixed Exercise2

--- Chapter2/Exercise3.cpp
+++ Chapter2/Exercise3.cpp
@@ -2,37 +2,50 @@
 
 class vector3d
 {
-private :
-    int x;
-    decltype(x) y;
-    decltype(x) z;
-
 public :
-    decltype(x) get_vector_x ();
-    decltype(y) get_vector_y ();
-    decltype(z) get_vector_z ();
+    using var_type = int;
+    
+    var_type get_vector_x ();
+    var_type get_vector_y ();
+    var_type get_vector_z ();
+    void set_vector_x (var_type x);
+    void set_vector_y (var_type y);
+    void set_vector_z (var_type z);
 
-    vector3d (int _x, int _y, int _z)
-    {
-        x = _x;
-        y = _y;
-        z = _z;
-    }
+private :
+    var_type x;
+    var_type y;
+    var_type z;
 };
 
-decltype(vector3d::x) vector3d::get_vector_x()
+vector3d::var_type vector3d::get_vector_x()
 {
     return x;
 }
 
-decltype(vector3d::y) vector3d::get_vector_y()
+vector3d::var_type vector3d::get_vector_y()
 {
     return y;
 }
 
-decltype(vector3d::z) vector3d::get_vector_z()
+vector3d::var_type vector3d::get_vector_z()
 {
     return z;
+}
+
+void vector3d::set_vector_x (var_type new_x)
+{
+    x = new_x;
+}
+
+void vector3d::set_vector_y (var_type new_y)
+{
+    y = new_y;
+}
+
+void vector3d::set_vector_z (var_type new_z)
+{
+    z = new_z;
 }
 
 int main ()
@@ -43,7 +56,11 @@ int main ()
     std::cin >> y;
     std::cin >> z;
 
-    vector3d v = vector3d(x, y, z);
+    vector3d v = vector3d();
+    v.set_vector_x(x);
+    v.set_vector_y(y);
+    v.set_vector_z(z);
+    
 
     std::cout << "x = " << v.get_vector_x() << std::endl;
     std::cout << "y = " << v.get_vector_y() << std::endl;
