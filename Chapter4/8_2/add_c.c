e68419962dc2a30a98a74a6d6b2ada235e47ff4c
taniguchi <hogehoge>
2021-11-12 17:44:24 +0900

    Add 4.8

---
+++ Chapter4/8_2/add_c.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int add_c (int a, int b)
+{
+    return a + b;
+}
---
+++ Chapter4/8_2/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int add_c(int a, int b);
+
+int main ()
+{
+    std::cout << add_c(1, 2) << std::endl;
+}
