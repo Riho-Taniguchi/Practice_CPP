50561fe3ead0e1620d3a729471fbcb54c9816f03
taniguchi <hogehoge>
2021-12-14 18:53:38 +0900

    Add 8.6

---
+++ Chapter8/6_1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <fstream>
+
+int main ()
+{
+    int file_size = 15;
+    std::string file_name = "sample.bin";
+
+    std::ofstream out{file_name, std::ios::binary};
+    for (int i = 0; i < file_size; i++)
+    {
+        out.put('a' + i);
+    }
+    out.close();
+
+    std::ifstream in{file_name, std::ios::binary};
+    char buffer[file_size];
+    in.read(buffer, file_size);
+    std::cout << buffer << std::endl;
+}
